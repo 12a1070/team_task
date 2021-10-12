c9a2ce45dbe7e473e041aee30023ef3e70b27ef7
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-10-12 15:58:36 +0900

    アジェンダの削除機能を実装

---
+++ app/mailers/agenda_deleted_mailer.rb
@@ -0,0 +1,6 @@
+class AgendaDeletedMailer < ApplicationController
+  def agenda_deleted_mail(agenda)
+    @members = agenda.team.members
+      mail to: @members.map(&:email).join(','),subject: 'アジェンダ削除の通知'
+  end
+end
