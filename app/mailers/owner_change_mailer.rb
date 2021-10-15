b9e0e9a337c16f347df9a976556ef451e268dc2d
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-10-15 16:16:35 +0900

    view実装

--- app/mailers/owner_change_mailer.rb
+++ app/mailers/owner_change_mailer.rb
@@ -2,6 +2,6 @@ class OwenerChangeMailer <AgendaDeletedMailer
   default from: 'from@example.com'
 
   def owner_change_mail(user)
-    mail to: user.email,subject:'チームのオーナーの権限付与'
+    mail to: user.email,subject:'チームのオーナーの権限付与しました'
   end
 end
